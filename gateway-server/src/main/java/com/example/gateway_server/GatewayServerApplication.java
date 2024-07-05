1db29aa639a2903d7e26f1d9a5c0df1cab7d7c8d
vamsi <vamsiplaty@gmail.com>
2024-07-05 21:35:52 +0530

    circuit breaker integrated

--- gateway-server/src/main/java/com/example/gateway_server/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/example/gateway_server/GatewayServerApplication.java
@@ -6,30 +6,35 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServerApplication.class, args);
-	}
-	@Bean
-	public RouteLocator eCommerceRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
-		return routeLocatorBuilder.routes()
-				.route(p -> p
-						.path("/vcommerce/order-service/**")
-						.filters( f -> f.rewritePath("vcommerce/order-service(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-						.uri("lb://order-service"))
-				.route(p-> p.path("/vcommerce/product-service/**")
-						.filters( f -> f.rewritePath("vcommerce/product-service(?<segment>.*)","/${segment}")
-								.circuitBreaker(config -> config.setName("gatewayservercircuitbreaker")
-								.setFallbackUri("forward:/fallback/product"))
-						.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-						.uri("lb://product-service")).build();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator eCommerceRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/vcommerce/order-service/**")
+                        .filters(f -> f.rewritePath("vcommerce/order-service(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://order-service"))
+
+                .route(p -> p.path("/vcommerce/product-service/**")
+                        .filters(f -> f.rewritePath("vcommerce/product-service(?<segment>.*)", "/${segment}")
+                                .circuitBreaker(config -> config.setName("gatewayservercircuitbreaker")
+                                        .setFallbackUri("forward:/fallback/product"))
+                                .retry(retryConfig -> retryConfig.setMethods(HttpMethod.GET).setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://product-service")).build();
+    }
 
 }
